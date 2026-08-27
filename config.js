b94e57b924b85e2f41533756116e1bba97cab998
anandufx005-commits <anandufx005@gmail.com>
2026-08-27 10:18:53 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// ==========================================
+// APNI ADSTERRA DIRECT LINK YAHAN PASTE KAREIN
+// ==========================================
+
+var OFFER_URL = "https://www.profitableratecpmnetwork.com/d9891b95t?key=08a56a129f53a4086f912dfc01592637";
